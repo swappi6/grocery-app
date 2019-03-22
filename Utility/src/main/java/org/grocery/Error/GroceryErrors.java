da67f1d6eb58aaab5d0921a02c7c8674b7191513
shashi.prakash <shashi.prakash@olacabs.com>
2019-03-22 15:01:40 +0530

    storage

--- Utility/src/main/java/org/grocery/Error/GroceryErrors.java
+++ Utility/src/main/java/org/grocery/Error/GroceryErrors.java
@@ -6,4 +6,5 @@ public class GroceryErrors {
     public static final GroceryError UNKNOWN_ERRROR = new GroceryError("UNKNOWN_ERRROR", "Error while processing the request");
     public static final GroceryError INVALID_REFRESH_TOKEN = new GroceryError("INVALID_REFRESH_TOKEN", "Invalid refresh token");
     public static final GroceryError INVALID_USER = new GroceryError("INVALID_USER", "User Doesn't Exist");
+    public static final GroceryError S3_UPLOAD_ERROR = new GroceryError("S3_UPLOAD_ERROR", " Error uploading image");
 }    
