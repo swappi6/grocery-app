a46a1d063fdd2d0123fe557333ab81027c40e27e
shashi.prakash <shashi.prakash@olacabs.com>
2019-05-29 17:45:25 +0530

    hbkjn

--- Orders/src/main/java/org/grocery/Orders/OrderController.java
+++ Orders/src/main/java/org/grocery/Orders/OrderController.java
@@ -44,11 +44,8 @@ public class OrderController {
 	@Path("/searchById")
 	public Response searchOrderById(@QueryParam(value = "orderId")long orderId)throws GroceryException, Exception{
 		ResponseBuilder responseBuilder = javax.ws.rs.core.Response.ok();
-		//SearchOrderResponse response = new SearchOrderResponse();
-		OrderResponseDetails orders=orderService.searchOrderById(orderId);
-		//response.setOrder(order);
-		return responseBuilder.entity(orders).build();
-		//return responseBuilder.entity(response).build();
+		OrderResponseDetails order =orderService.searchOrderById(orderId);
+		return responseBuilder.entity(order).build();
 	}
 
 	@GET
@@ -56,36 +53,30 @@ public class OrderController {
 	@Path("/search-order-by-user-id")
 	public Response searchOrderByUserId(@QueryParam(value = "userId")long userId)throws GroceryException{
 		ResponseBuilder responseBuilder = javax.ws.rs.core.Response.ok();
-//		SearchOrderResponse response = new SearchOrderResponse();
 		List<OrderResponseDetails> orders =orderService.searchOrderByUserId(userId);
-//		
-//		
-//		List<Item> itemDetails = itemService.getItems(26L);
-//		User user = userService.getUserById(orders.get(0).getUserId());
-//		response.setOrder(orders);
-//		response.setItems(itemDetails);
-//		response.setUser(user);
-		return responseBuilder.entity(orders).build();
+		OrderList orderList = new OrderList();
+        orderList.setOrders(orders);
+		return responseBuilder.entity(orderList).build();
 	}
 	@GET
 	@UnitOfWork
 	@Path("/search-order-by-date")
 	public Response searchOrderByDate(@QueryParam(value = "date")Long date) throws GroceryException, ParseException{
 		ResponseBuilder responseBuilder = javax.ws.rs.core.Response.ok();
-		//SearchOrderResponse response = new SearchOrderResponse();
-		List<OrderResponseDetails> order = orderService.searchOrderByDate(date);
-		//response.setOrder(order);
-		return responseBuilder.entity(order).build();
+		List<OrderResponseDetails> orders = orderService.searchOrderByDate(date);
+		OrderList orderList = new OrderList();
+        orderList.setOrders(orders);
+		return responseBuilder.entity(orderList).build();
 	}
 	@GET
 	@UnitOfWork
 	@Path("/search-order")
 	public Response searchActiveOrder(@QueryParam(value = "status") @NotNull List<OrderStatus> status) throws GroceryException{
 		ResponseBuilder responseBuilder = javax.ws.rs.core.Response.ok();
-		//SearchOrderResponse response = new SearchOrderResponse();
-		List<OrderResponseDetails> order = orderService.searchActiveOrder(status);
-		//response.setOrder(order);
-		return responseBuilder.entity(order).build();
+		List<OrderResponseDetails> orders = orderService.searchActiveOrder(status);
+		OrderList orderList = new OrderList();
+		orderList.setOrders(orders);
+		return responseBuilder.entity(orderList).build();
 	}
 	@POST
 	@UnitOfWork
@@ -103,13 +94,14 @@ public class OrderController {
 		orderService.updateOrder(updateOrder, orderId);
 		return responseBuilder.build();
 	}
-		@DELETE
-		@UnitOfWork
-		@Path("/delete-order/{orderId}")
-		public Response deleteOrder(@PathParam(value = "orderId") Long orderId) throws GroceryException {
-			ResponseBuilder responseBuilder = Response.noContent();
-			orderService.delete(orderId);
-			return responseBuilder.build();
-		}
+		
+	@DELETE
+    @UnitOfWork
+    @Path("/delete-order/{orderId}")
+    public Response deleteOrder(@PathParam(value = "orderId") Long orderId) throws GroceryException {
+        ResponseBuilder responseBuilder = Response.noContent();
+        orderService.delete(orderId);
+        return responseBuilder.build();
+    }
 
 }
